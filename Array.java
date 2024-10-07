cf41e8681a037abc86df313548cccbf10e9b6e29
ClaroJp <jpaul.claro@gmail.com>
2024-10-07 19:47:09 +0800

    added code

---
+++ Array.java
@@ -0,0 +1,10 @@
+public class Array{
+    public static void main(String [] args){
+
+        int [] array = [1, 2, 3];
+
+        for(int i = 0; i < array.length; i++){
+            system.out.print(array[i] + " ")
+        }
+    }
+}
